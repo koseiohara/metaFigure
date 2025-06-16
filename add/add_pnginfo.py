9fd4d0e8f6f2e56e1b193e1421f9755ba612a989
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2025-06-16 17:31:15 +0900

    bug fix in add_pnginfo.py
    2 or more metadata can be added

--- add/add_pnginfo.py
+++ add/add_pnginfo.py
@@ -3,6 +3,10 @@ from PIL import Image, PngImagePlugin
 def add_pnginfo(target, key, text):
     png  = Image.open(target)
     meta = PngImagePlugin.PngInfo()
+
+    for key_old, value_old in png.info.items():
+        meta.add_text(key_old, value_old)
+
     meta.add_text(key, text)
 
     png.save(target, pnginfo=meta)
